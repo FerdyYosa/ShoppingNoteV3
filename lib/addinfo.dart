ce30690969076e4a191d9bc10b73bcc3629c8d79
Muhammad Ferdy Yosa <ferdyzecter@gmail.com>
2019-09-21 19:39:03 +0700

    Add code to add TextFormField "Nama", "Deskripsi" and "Total belanja", and also add code to add two Raised Button, and also add code to make the two Raised Button get in one Row.

--- lib/addinfo.dart
+++ lib/addinfo.dart
@@ -9,10 +9,61 @@ class _IsiDataState extends State<IsiData> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      backgroundColor: Colors.blue,
-      leading: Icon(Icons.arrow_back),
-      title: Text("Tambah Catatan"),
-    ));
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        leading: Icon(Icons.arrow_back),
+        title: Text("Tambah Catatan"),
+      ),
+      body: Form(
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(10),
+              child: TextFormField(
+                decoration: InputDecoration(labelText: "Tanggal"),
+                onTap: () async {
+                  final datePick = await showDatePicker(
+                      context: context,
+                      initialDate: new DateTime.now(),
+                      firstDate: new DateTime(2019),
+                      lastDate: new DateTime(2026));
+                },
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: TextFormField(
+                decoration: InputDecoration(labelText: "Nama:"),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: TextFormField(
+                decoration: InputDecoration(labelText: "Deskripsi:"),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: TextFormField(
+                decoration: InputDecoration(labelText: "Total belanja:"),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                RaisedButton(
+                  child: Text("Batal"),
+                  onPressed: () {},
+                ),
+                RaisedButton(
+                  child: Text("Simpan"),
+                  onPressed: () {},
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
